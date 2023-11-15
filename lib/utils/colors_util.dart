f1cf8170b625d173c9ad1d36a1eb76ecd8d1e3bf
Paolo17A <paoloandal@gmail.com>
2023-11-15 15:14:31 +0800

    Initial commit

---
+++ lib/utils/colors_util.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color midnightExtress = Color.fromARGB(255, 36, 41, 67);
+  static const Color sweetCorn = Color.fromARGB(255, 244, 219, 115);
+}
